47f3d27e76b1ccc476aa26178df9c9744eb76b0f
Silas-py <2669128599@qq.com>
2022-07-25 12:35:36 +0800

    finish lab0:webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -8,7 +8,20 @@ using namespace std;
 
 void get_URL(const string &host, const string &path) {
     // Your code here.
-
+    TCPSocket sock;
+    sock.connect(Address(host, "http"));
+    // GET /hello HTTP/1.1
+    // auto sock2 = sock.accept();
+    sock.write("GET " + path + " HTTP/1.1\r\n");
+    sock.write("Host: " + host + "\r\n");
+    sock.write("Connection: close\r\n\r\n");
+    // sock.shutdown(SHUT_WR);
+    // cout << "here" << endl;
+    
+    while (!sock.eof()) {
+        cout << sock.read();
+    }
+    sock.close();   
     // You will need to connect to the "http" service on
     // the computer whose name is in the "host" string,
     // then request the URL path given in the "path" string.
@@ -17,8 +30,8 @@ void get_URL(const string &host, const string &path) {
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    // cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    // cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main(int argc, char *argv[]) {
