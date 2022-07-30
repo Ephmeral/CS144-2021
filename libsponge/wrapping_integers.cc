d13614680c2bc39e70ba4576f7a3ad08dfe93d57
Silas-py <2669128599@qq.com>
2022-07-30 23:31:36 +0800

    warmup

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint64_t res = (n + static_cast<uint64_t> (isn.raw_value())) % (1UL << 32);
+    return WrappingInt32{static_cast<uint32_t> (res)};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +29,18 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t absqeno = static_cast<uint64_t>(n.raw_value() - isn.raw_value());
+    if (checkpoint <= absqeno) return absqeno;
+    uint64_t k = (checkpoint - absqeno) >> 32;
+    uint64_t lowbit = ((checkpoint - absqeno) << 32) >> 32;
+    if (lowbit < (1UL << 31)) {
+      return absqeno + k * (1UL << 32);
+    }
+    return absqeno + (k + 1) * (1UL << 32);
+    //高级写法
+    // int32_t interval = n - wrap(checkpoint, isn);
+    // int64_t result = checkpoint + interval;
+    // if (result >= 0)
+    //     return result;
+    // return result + (1ul << 32);
 }
